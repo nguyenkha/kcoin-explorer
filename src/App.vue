e4cbc4d201f005d060ebae20eabd254aaa182d2d
Kha N. Do <donguyenkha@gmail.com>
2017-12-24 00:11:20 +0900

    Change color to red

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="md" type="dark" variant="info">
+    <b-navbar toggleable="md" type="dark" variant="danger">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
       <b-navbar-brand href="#">KCoin Explorer</b-navbar-brand>
     </b-navbar>
